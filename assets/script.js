efc611b389666bd5831f7834947808679fbff653
chris interiano <cinteriano@live.com>
2021-02-22 23:20:22 -0800

    still working on jquery

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,37 @@
-$(documt).ready(function(){
-    $(".saveBtn").on("click," function(){
+$(documt).ready(function () {
+    $(".saveBtn").on("click" , function () {
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        localStorage.setItem(time,value)
+        localStorage.setItem(time, value)
     });
 
 
-    function hourUpdater(){
+    function hourUpdater() {
         var currentHour = moment().hours();
 
-        $(".time-block").each(function(){
+        $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
-            if(blockHour < currentHour) {
-                $
-
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
 
             }
-
-
-        })
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        });
     }
 
+    hourUpdater();
+
+    var intrerval = setInterval(hourUpdater, 15000);
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 
 
 
@@ -56,4 +54,7 @@ $(documt).ready(function(){
 
 
 
-})
+
+
+
+});
