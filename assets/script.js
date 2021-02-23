2e011efa7b25a4f80fd8fe44dc251664808a553c
chris interiano <cinteriano@live.com>
2021-02-22 23:36:47 -0800

    working on styling

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-$(documt).ready(function () {
+$(document).ready(function () {
     $(".saveBtn").on("click" , function () {
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -32,29 +32,14 @@ $(documt).ready(function () {
 
     var intrerval = setInterval(hourUpdater, 15000);
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 });
